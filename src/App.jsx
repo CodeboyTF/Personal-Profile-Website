6253701cb7a8d5c766d3736f56676283e1f1383d
fmakharamedzha <fmakharamedzha@csir.co.za>
2025-07-22 17:51:54 +0200

    updated new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavbarMenu from "./GlobalComponents/navbar";
+import UrlIcon from "./GlobalComponents/UrlIcon";
 
 function App() {
   return (
     <>
       <NavbarMenu />
+      <UrlIcon />
     </>
   );
 }
