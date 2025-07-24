7e3812cbd3cf34cd6226c050012fc61ead4ec6f9
fmakharamedzha <fmakharamedzha@csir.co.za>
2025-07-24 15:46:18 +0200

    updated npm modules

--- src/Pages/About_Page/Profile.jsx
+++ src/Pages/About_Page/Profile.jsx
@@ -1,5 +1,8 @@
 import Contribution from "./Contribution_Button";
 import DownloadCv from "./Download_Button";
+import Stat_bar from "./Stats_Bar";
+
+// import Contribution from "Pages/About_Page/Contribution_Button.jsx";
 
 function Profile() {
   return (
@@ -9,13 +12,19 @@ function Profile() {
           <h1>About Me</h1>
         </div>
         <div className="card-content">
-          I am Tebogo Makharamedzha, a software developer passionate about
-          building modern and efficient web applications...
+          I am Tebogo Makharamedzha, a software developer passionate about building modern and efficient web applications...
+
           <div className="button-stack">
             <div className="button-downloadcv">
               <DownloadCv />
+            </div>
+            <div className="button-contribution">
               <Contribution />
             </div>
+          </div>
+
+                    <div className="Stats">
+            <Stat_bar />
           </div>
         </div>
       </div>
@@ -24,3 +33,4 @@ function Profile() {
 }
 
 export default Profile;
+
---
+++ src/Pages/About_Page/Stats_Bar.jsx
@@ -0,0 +1,51 @@
+import { FaProjectDiagram } from "react-icons/fa";
+import { GiDiploma } from "react-icons/gi";
+import { MdWork } from "react-icons/md";
+import { FaLink } from "react-icons/fa";
+
+function Stat_bar() {
+  return (
+    <div className="stats-container-card">
+      <div
+        className="stats-bar-item"
+        onClick={() => (window.location.href = "./education.jsx")}
+      >
+        <div className="stats-row">
+          <FaProjectDiagram className="stats-icon" />
+          <p className="stat-number">12</p>
+        </div>
+        <h4 className="stat-title">Total Projects</h4>
+        <h5 className="stat-label">Total Built Projects</h5>
+        <FaLink className="link-icon" />
+      </div>
+
+      <div
+        className="stats-bar-item"
+        onClick={() => (window.location.href = "./certificates.jsx")}
+      >
+        <div className="stats-row">
+          <GiDiploma className="stats-icon" />
+          <p className="stat-number">26</p>
+        </div>
+        <h4 className="stat-title">Certificates</h4>
+        <h5 className="stat-label">Professional Skills Validated</h5>
+        <FaLink className="link-icon" />
+      </div>
+
+      <div
+        className="stats-bar-item"
+        onClick={() => (window.location.href = "./experience.jsx")}
+      >
+        <div className="stats-row">
+          <MdWork className="stats-icon" />
+          <p className="stat-number">1½</p>
+        </div>
+        <h4 className="stat-title">Experience</h4>
+        <h5 className="stat-label">Years of Experience</h5>
+        <FaLink className="link-icon" />
+      </div>
+    </div>
+  );
+}
+
+export default Stat_bar;
