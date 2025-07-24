2786ede4b9eb68656924ab576c02bf84e9ff61b3
fmakharamedzha <fmakharamedzha@csir.co.za>
2025-07-24 14:49:40 +0200

    updated

--- src/Pages/About_Page/Profile.jsx
+++ src/Pages/About_Page/Profile.jsx
@@ -2,28 +2,25 @@ import Contribution from "./Contribution_Button";
 import DownloadCv from "./Download_Button";
 
 function Profile() {
-    return (
-        <>
-<div className="about-content-card">
-    <div className="card-header">
-      <h1>About Me</h1>
-    </div>
-    <div className="card-content">
-      I am Tebogo Makharamedzha, a software developer passionate about building modern and efficient web applications...
-<div className="button-stack">
-  <div className="button-downloadcv">
-    <DownloadCv />
-
-    <Contribution />
-  </div>
-  </div>
-    </div>
-  </div>
-
-
-
-        </>
-    );
+  return (
+    <>
+      <div className="about-content-card">
+        <div className="card-header">
+          <h1>About Me</h1>
+        </div>
+        <div className="card-content">
+          I am Tebogo Makharamedzha, a software developer passionate about
+          building modern and efficient web applications...
+          <div className="button-stack">
+            <div className="button-downloadcv">
+              <DownloadCv />
+              <Contribution />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Profile;
