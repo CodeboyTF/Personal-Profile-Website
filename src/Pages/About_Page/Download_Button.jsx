d80117858f34c43e9d6a63be512de9ccc3741272
fmakharamedzha <fmakharamedzha@csir.co.za>
2025-07-24 13:13:45 +0200

    added a cv download button

---
+++ src/Pages/About_Page/Download_Button.jsx
@@ -0,0 +1,18 @@
+function DownloadCv() {
+  const downloadCV = () => {
+    const link = document.createElement("a");
+    link.href = "Fhatuwani CV.pdf"; // Replace with your actual CV file path
+    link.download = "Fhatuwani_Makharamedzha_CV.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <>
+      <button onClick={downloadCV}>Download CV</button>
+    </>
+  );
+}
+
+export default DownloadCv;
