db9afee4288ed0f222b3b9e59dfaed957fb3e74f
fmakharamedzha <fmakharamedzha@csir.co.za>
2025-07-29 09:58:25 +0200

    updated work

--- src/Pages/About_Page/Description.jsx
+++ src/Pages/About_Page/Description.jsx
@@ -17,25 +17,22 @@ function WhatIDo() {
       title: "Integrated Services",
       desc: "I integrate third-party systems and APIs to ensure seamless communication between your tools, application and platforms.",
     },
-    {
-      icon: <FaUserTie />,
-      title: "Freelance Developer",
-      desc: "As a freelance developer, I take on versatile projects delivering tailored, scalable, and professional-grade solutions.",
-    },
   ];
 
   return (
     <div className="what-i-do-container">
       <h5>What I Do</h5>
-      {services.map((item, index) => (
-        <div key={index} className="what-i-do-card">
-          <div className="icon">{item.icon}</div>
-          <div className="text-content">
-            <h3>{item.title}</h3>
-            <p className="desc" data-fulltext={item.desc}>{item.desc}</p>
+      <div className="what-i-do-cards">
+        {services.map((item, index) => (
+          <div key={index} className="what-i-do-card">
+            <div className="icon">{item.icon}</div>
+            <div className="text-content">
+              <h3>{item.title}</h3>
+              <p className="desc">{item.desc}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
