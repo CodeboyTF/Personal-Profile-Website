7f6f6705280d464a175cd239359daee92b10253d
fmakharamedzha <fmakharamedzha@csir.co.za>
2025-08-07 19:38:51 +0200

    updated photo gallery2 jsx

--- src/Pages/Gallery_Page/Gallery2.jsx
+++ src/Pages/Gallery_Page/Gallery2.jsx
@@ -4,12 +4,12 @@ import './Gallery2.css';
 
 function Gallery2() {
   const photos = [
-    { src: '/images/pro1.jpg', caption: 'Public Talk 2023', rotation: '-5deg', top: '5%', left: '10%' },
-    { src: '/images/pro2.jpg', caption: 'Networking Event', rotation: '4deg', top: '20%', left: '35%' },
-    { src: '/images/pro3.jpg', caption: 'Award Moment', rotation: '-3deg', top: '45%', left: '60%' },
-    { src: '/images/pro4.jpg', caption: 'Team Collaboration', rotation: '6deg', top: '30%', left: '70%' },
-    { src: '/images/pro5.jpg', caption: 'Conference Panel', rotation: '-8deg', top: '60%', left: '15%' },
-    { src: '/images/pro6.jpg', caption: 'Workshop Host', rotation: '5deg', top: '75%', left: '50%' },
+    { src: '/images/pro6.jpg', caption: 'Team Collaboration', rotation: '6deg', top: '30%', left: '70%' },
+    { src: '/images/pro4.jpg', caption: 'Networking Event', rotation: '4deg', top: '20%', left: '35%' },
+    { src: '/images/pro1.jpg', caption: 'Conference Panel', rotation: '-8deg', top: '60%', left: '15%' },
+    { src: '/images/pro5.jpg', caption: 'Award Moment', rotation: '-3deg', top: '45%', left: '60%' },
+    { src: '/images/pro3.jpg', caption: 'Public Talk 2023', rotation: '-5deg', top: '5%', left: '10%' },
+    { src: '/images/pro2.jpg', caption: 'Workshop Host', rotation: '5deg', top: '75%', left: '50%' },
   ];
 
   return (
