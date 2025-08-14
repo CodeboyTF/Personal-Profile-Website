04b55f282680ef23092a2e6a07cd628b71094a0e
fmakharamedzha <fmakharamedzha@csir.co.za>
2025-08-14 20:38:40 +0200

    Done Education Feature jsx

--- src/Pages/Education/Education.jsx
+++ src/Pages/Education/Education.jsx
@@ -2,25 +2,36 @@ import React from "react";
 import "./Education.css";
 const EDUCATION = [
   {
-    school: "University of Example",
-    logo: "/Logos/UJlogo.png",
-    course: "Bachelor of Science",
-    major: "Computer Science",
-    duration: "2018 – 2021",
+    school: "Lamula Jubilee Secondary School",
+    logo: "/Logos/Lamulalogo.jpg",
+    qualification: "National Senior Certificate",
+    course: "Matric",
+    major: "",
+    duration: "2015 – 2020",
   },
   {
-    school: "Example Institute of Technology",
+    school: "Vaal University of Technology",
     logo: "/Logos/Vutlogo.jpg",
-    course: "Diploma",
-    major: "Information Systems",
-    duration: "2016 – 2018",
+    qualification: "National Diploma",
+    course: "Information Technology",
+    major: "Software Development / Business Analysis",
+    duration: "2021 – 2023",
   },
   {
-    school: "Sample High School",
-    logo: "/logos/sample-high.png",
-    course: "National Senior Certificate",
-    major: "Science Stream",
-    duration: "2011 – 2015",
+    school: "CSIR",
+    logo: "/Logos/Csirlogo1.jpg",
+    qualification: "Software Developer / CyberAnalyst",
+    course: "Internship",
+    major: "ReactJs| Django| ExpressJs| DataBases",
+    duration: "2024 – Present",
+  },
+    {
+    school: "University of Johannesburg",
+    logo: "/Logos/UJlogo.png",
+    qualification: "Advanced Diploma",
+    course: "Busines Information Technology",
+    major: "(Aspiring)",
+    duration: "2026 – 2026",
   },
 ];
 const Education = () => {
@@ -43,6 +54,7 @@ const Education = () => {
                 />
                 <div className="edu-text">
                   <h3 className="edu-school">{item.school}</h3>
+                  <p className="edu-course">{item.qualification}</p>
                   <p className="edu-course">{item.course}</p>
                   <p className="edu-major">{item.major}</p>
                   <p className="edu-duration">{item.duration}</p>
